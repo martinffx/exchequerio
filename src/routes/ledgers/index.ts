bdfefb4457bcfeabcbe1329a6c0a73ac11f4a9be
Martin C. Richards <martinffx@gmail.com>
2025-12-06 11:42:44 +0100

    cleanup routes

--- src/routes/ledgers/index.ts
+++ src/routes/ledgers/index.ts
@@ -5,30 +5,26 @@ import { LedgerAccountRoutes } from "./LedgerAccountRoutes";
 import { LedgerAccountSettlementRoutes } from "./LedgerAccountSettlementRoutes";
 import { LedgerAccountStatementRoutes } from "./LedgerAccountStatementRoutes";
 import { LedgerRoutes } from "./LedgerRoutes";
-import { LedgerTransactionEntriesRoutes } from "./LedgerTransactionEntriesRoutes";
 import { LedgerTransactionRoutes } from "./LedgerTransactionRoutes";
 
-const LedgerRouterPlugin: FastifyPluginAsync = async server => {
-	await server.register(LedgerRoutes);
-	await server.register(LedgerTransactionRoutes, {
-		prefix: "/:ledgerId/transactions",
-	});
-	await server.register(LedgerTransactionEntriesRoutes, {
-		prefix: "/transactions/entries",
-	});
-	await server.register(LedgerAccountRoutes, { prefix: "/:ledgerId/accounts" });
-	await server.register(LedgerAccountCategoryRoutes, {
-		prefix: "/accounts/categories",
-	});
-	await server.register(LedgerAccountSettlementRoutes, {
-		prefix: "/accounts/settlements",
-	});
-	await server.register(LedgerAccountStatementRoutes, {
-		prefix: "/accounts/statements",
-	});
-	await server.register(LedgerAccountBalanceMonitorRoutes, {
-		prefix: "/accounts/balance-monitors",
-	});
+const LedgerRouterPlugin: FastifyPluginAsync = async (server) => {
+  await server.register(LedgerAccountCategoryRoutes, {
+    prefix: "/:ledgerId/accounts/categories",
+  });
+  await server.register(LedgerAccountSettlementRoutes, {
+    prefix: "/:ledgerId/accounts/:accountId/settlements",
+  });
+  await server.register(LedgerAccountStatementRoutes, {
+    prefix: "/:ledgerId/accounts/:accountId/statements",
+  });
+  await server.register(LedgerAccountBalanceMonitorRoutes, {
+    prefix: "/:ledgerId/accounts/:accountId/balance-monitors",
+  });
+  await server.register(LedgerAccountRoutes, { prefix: "/:ledgerId/accounts" });
+  await server.register(LedgerTransactionRoutes, {
+    prefix: "/:ledgerId/transactions",
+  });
+  await server.register(LedgerRoutes);
 };
 
 export { LedgerRouterPlugin };
